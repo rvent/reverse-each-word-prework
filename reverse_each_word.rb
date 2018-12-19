1b27ab924d72dcc7da74cf6438141df0f010e8a2
Rene Ventura <venturar729@gmail.com>
2018-12-19 04:10:25 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split(" ")
   reversed_word_array = word_array.collect do |word|
     reversed_char_array = word.split("").reverse
-    reversed_word = reversed_char_array.join("")
+    reversed_char_array.join("")
   end
   reversed_word_array.join(" ")
 end
